1a481a427e3221b5feefd8ee978c4f3656c79ed8
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-03-18 00:22:23 +0700

    Auth controllers

---
+++ Controllers/AuthorsController.cs
@@ -0,0 +1,23 @@
+﻿using BooksDirectoryAPI.Models;
+using BooksDirectoryAPI.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthorsController : ControllerBase
+{
+    private readonly IAuthorRepository _repo;
+
+    public AuthorsController(IAuthorRepository repo)
+    {
+        _repo = repo;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+        => Ok(await _repo.GetAllAsync());
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Author author)
+        => Ok(await _repo.CreateAsync(author));
+}
