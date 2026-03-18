586b7483add10d19d87812f0e67ab1655e33b8ab
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-03-18 13:09:58 +0700

    Dtos books service

---
+++ DTOs/BookDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BooksDirectoryAPI.DTOs
+{
+    public class BookDto
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? AuthorName { get; set; }
+        public string? GenreName { get; set; }
+    }
+}
+
