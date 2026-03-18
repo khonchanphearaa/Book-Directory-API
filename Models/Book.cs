b914f41fcee6ac2a25518784ab4d8ea2ef8a7587
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-03-18 22:58:46 +0700

    Models of books

---
+++ Models/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BooksDirectoryAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public int Author_Id { get; set; }
+        public int Genre_Id { get; set; }
+    }
+}
+
