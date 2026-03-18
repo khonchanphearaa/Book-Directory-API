2328ef38278929f856e09d1fba271bc1fce1dbd5
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-03-18 13:08:56 +0700

    Connection mysqlController

---
+++ Data/DbConnectionFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MySqlConnector;
+using System.Data;
+
+namespace BooksDirectoryAPI.Data
+{
+    using MySqlConnector;
+    using System.Data;
+
+    public class DbConnectionFactory
+    {
+        private readonly string? _connectionString;
+
+        public DbConnectionFactory(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("DefaultConnection");
+        }
+
+        public IDbConnection CreateConnection()
+            => new MySqlConnection(_connectionString);
+    }
+}
